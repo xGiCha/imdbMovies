52055cfa7efe565a88ded62915268101379a1702
ichatzigiannis <ichatzigiannis@cognity.gr>
2021-07-22 12:57:07 +0300

    exoplayer

--- app/src/main/java/gr/imdb/movies/ui/fragments/MoviesDetailsFragment.kt
+++ app/src/main/java/gr/imdb/movies/ui/fragments/MoviesDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,6 +17,16 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import at.huber.youtubeExtractor.VideoMeta
+import at.huber.youtubeExtractor.YouTubeExtractor
+import at.huber.youtubeExtractor.YtFile
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.MergingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.util.Util
 import com.squareup.picasso.Picasso
 import gr.imdb.movies.R
 import gr.imdb.movies.adapters.ReviewsAdapter
@@ -40,6 +51,10 @@ class MoviesDetailsFragment() : Fragment() {
     private var myMovie: Movie? = null
     private lateinit var moviesEntity: MoviesEntity
     private var mReviewList: MutableList<EntityReview> = mutableListOf()
+    private var player: SimpleExoPlayer?= null
+    private var playWhenReady = true
+    private  var currentWindow = 0
+    private  var playbackPosition: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         movieViewModel = ViewModelProvider(requireActivity()).get(MovieViewModel::class.java)
@@ -126,6 +141,7 @@ class MoviesDetailsFragment() : Fragment() {
     }
 
     private fun init() {
+        initPlayer()
         args.movie.let { movie ->
             myMovie = movie
             myMovie?.let {
@@ -154,6 +170,81 @@ class MoviesDetailsFragment() : Fragment() {
         binding.reviewTxtV.bodyInfoTxtV.visibility = View.GONE
 
         setUpAdapters()
+    }
+
+    private fun initPlayer() {
+        player = SimpleExoPlayer.Builder(requireContext()).build()
+        binding.exoplayer.player = player
+
+        val videoUrl = "https://www.youtube.com/watch?v=yRCznT1dEk8"
+        object: YouTubeExtractor(requireContext()){
+            override fun onExtractionComplete(
+                ytFiles: SparseArray<YtFile>?,
+                videoMeta: VideoMeta?
+            ) {
+                if(ytFiles != null){
+                    val itag = 137
+                    val audioTag = 140
+                    val videoUrl = ytFiles[itag].url
+                    val audioUrl = ytFiles[audioTag].url
+
+                    val audioSource: MediaSource = ProgressiveMediaSource
+                        .Factory(DefaultHttpDataSource.Factory())
+                        .createMediaSource(MediaItem.fromUri(audioUrl))
+
+                    val videoSource: MediaSource = ProgressiveMediaSource
+                        .Factory(DefaultHttpDataSource.Factory())
+                        .createMediaSource(MediaItem.fromUri(videoUrl))
+
+                    player!!.setMediaSource(MergingMediaSource(
+                        true, videoSource, audioSource
+                    ), true)
+                    player!!.prepare()
+                    player!!.playWhenReady = playWhenReady
+                    player!!.seekTo(currentWindow, playbackPosition)
+                }
+            }
+
+        }.extract(videoUrl, false, true)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(Util.SDK_INT >=26)
+            initPlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(player == null){
+            initPlayer()
+            hideSystemUI()
+        }
+    }
+
+    private fun hideSystemUI() {
+        binding.exoplayer.systemUiVisibility =(
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN
+                )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        releasePlayer()
+    }
+
+    private fun releasePlayer() {
+        if(player != null){
+            playWhenReady = player!!.playWhenReady
+            playbackPosition = player!!.currentPosition
+            currentWindow = player!!.currentWindowIndex
+            player!!.release()
+            player = null
+        }
     }
 
     fun loadImageFromUrl(image: String){
